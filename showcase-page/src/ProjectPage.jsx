f15110f7d144f48fbab5c2430b744e09804f460f
Jan Seipel <janfseipel@gmail.com>
2025-02-19 22:29:31 +0100

    smartphone and screenshot images

--- showcase-page/src/ProjectPage.jsx
+++ showcase-page/src/ProjectPage.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Smartphone from "./assets/blank_smartphone_template.png"
 
 function ProjectPage({ title, description, image }) {
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <div className="flex flex-col md:flex-row items-center md:items-start">
-          {/* Bild */}
-          <div className="w-full md:w-1/2 mb-6 md:mb-0 md:mr-8">
-            <img 
-              src={image} 
-              alt={title} 
-              className="w-full h-auto rounded-lg shadow-lg"
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="flex flex-col md:flex-row items-center md:items-start">
+        {/* Bild */}
+        <div className="flex w-full md:w-1/2 mb-6 md:mb-0 md:mr-8 justify-center relative">
+          <div className="h-[80vh] aspect-[53/100] relative">
+            <img
+              src={Smartphone}
+              alt={title}
+              className="h-full"
+            />
+            <img
+              src={image}
+              alt={title}
+              className="h-[85%] w-[91.5%] top-[8%] left-[4.3%] absolute"
             />
           </div>
-          
-          {/* Titel und Beschreibung */}
-          <div className="w-full md:w-1/2">
-            <h1 className="text-3xl font-bold mb-4">{title}</h1>
-            <p className="text-lg text-gray-700">{description}</p>
-          </div>
         </div>
-        <Link to="/">
-        <div>zurück</div>
-        </Link>
+
+        {/* Titel und Beschreibung */}
+        <div className="w-full md:w-1/2">
+          <h1 className="text-3xl font-bold mb-4">{title}</h1>
+          <p className="text-lg text-gray-700">{description}</p>
+        </div>
       </div>
-    );
-  }
+      <Link to="/">
+        <div>zurück</div>
+      </Link>
+    </div>
+  );
+}
 
 export default ProjectPage
