ea8dfd9a0c5d126f0bb3d8790054bb6846ee1a89
Jan Seipel <janfseipel@gmail.com>
2025-02-23 07:13:32 +0100

    more real images

--- showcase-page/src/data/projects.js
+++ showcase-page/src/data/projects.js
@@ -1,13 +1,12 @@
 export const projects = [
   {
-    title: "My Maze Mazer",
+    title: "Maze Race",
     teaser: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat",
     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
     link: "project1",
-    demoLink: "https://www.swr.de",
+    demoLink: "https://jaeniboy.github.io/my-maze-mazer",
     repoLink: "https://www.github.com",
     image: "mazer_project.PNG",
-    // image: "https://picsum.photos/id/13/300/250",
     teaserImage: "mazer_teaser.PNG"
   },
   { 
@@ -15,26 +14,28 @@ export const projects = [
     teaser: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", 
     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", 
     link: "project2", 
-    demoLink: "https://www.swr.de",
+    demoLink: "https://jaeniboy.github.io/nonstop-destinations",
     repoLink: "https://www.github.com",
-    image: "https://picsum.photos/id/14/300/250" 
+    image: "destinations_project.PNG",
+    teaserImage: "destinations_teaser_2.png" 
   },
   { 
-    title: "Projekt 3", 
+    title: "Wiki Memo", 
     teaser: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat",
     description: "Kurze Beschreibung des Projekts 2", 
     link: "project3", 
-    demoLink: "https://www.swr.de",
+    demoLink: "https://jaeniboy.github.io/wiki-memo",
     repoLink: "https://www.github.com",
-    image: "https://picsum.photos/id/15/300/250" 
+    image: "memo_project.PNG",
+    teaserImage: "memo_teaser.PNG"
   },
-  { 
-    title: "Projekt 4", 
-    teaser: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat",
-    description: "Kurze Beschreibung des Projekts 2", 
-    link: "project4", 
-    demoLink: "https://www.swr.de",
-    repoLink: "https://www.github.com",
-    image: "https://picsum.photos/id/16/300/250" 
-  },
+  // { 
+  //   title: "Projekt 4", 
+  //   teaser: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat",
+  //   description: "Kurze Beschreibung des Projekts 2", 
+  //   link: "project4", 
+  //   demoLink: "https://www.swr.de",
+  //   repoLink: "https://www.github.com",
+  //   image: "https://picsum.photos/id/16/300/250" 
+  // },
 ];
